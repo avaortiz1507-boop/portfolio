0ecca98d43ff15314436be54a63ff6b568bd2a72
avaortiz1507-boop <avaortiz1507@gmail.com>
2025-11-18 11:16:59 -0800

    added more to contact form in js scipt

--- script.js
+++ script.js
@@ -28,11 +28,39 @@ document.addEventListener("DOMContentLoaded", function () {
       const email = document.querySelector("#email").value;
       const phone = document.querySelector("#phone").value;
       const message = document.querySelector("#message").value;
+      const errorMsg = document.querySelector(".error");
 
-      // log form data to console
-      console.log(
-        `Form submitted: ${firstName} ${lastName} ${email} ${phone} ${message} `
+      const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,3}/;
+
+      // validate email format
+      if (firstName === "" && lastName === "" && email === "") {
+        errorMsg.innerText = "Please enter required fields.";
+      }
+      if (!emailRegex.test(email)) {
+        errorMsg.innerText = "Please enter a valid email address.";
+        return;
+      }
+
+      // submit form
+
+      errorMsg.innerText = ""; // clear any previous error messages
+
+      // build the mailto form
+      const subject = encodeURIComponent(
+        `Contact Form Submission from ${firstName} ${lastName}`
       );
+      const body = encodeURIComponent(
+        `Name: ${firstName} ${lastName} \r\n` +
+          `Email: ${email} \r\n` +
+          `Phone: ${phone} \r\n` +
+          `Message: ${message}`
+      );
+
+      const recipient = "amortiz@batestech.com";
+      const mailtoLink = `mailto:${recipient}?subject=${subject}&body=${body}`;
+
+      // open the mailto link
+      window.location.href = mailtoLink;
 
       // reset form
       document.getElementById("contactForm").reset();
